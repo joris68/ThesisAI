9a57a2129def24aa3ce6457cf4f0cff9947851f9
joris68 <joris.gabrisch@web.de>
2024-03-16 20:26:50 +0100

    small changes

--- MicroServices/SharedResources/main.tf
+++ MicroServices/SharedResources/main.tf
@@ -1,3 +1,6 @@
+
+
+
 resource "aws_api_gateway_rest_api" "ThesisAIAPI" {
   name        = var.name
   description = var.description
@@ -11,7 +14,7 @@ resource "aws_api_gateway_rest_api" "ThesisAIAPI" {
 
 resource "aws_api_gateway_deployment" "api" {
   depends_on = [
-    aws_api_gateway_rest_api.ThesisAIAPI, # Make sure this references the correct API resource
+    aws_api_gateway_rest_api.ThesisAIAPI, 
   ]
 
   rest_api_id = aws_api_gateway_rest_api.ThesisAIAPI.id
